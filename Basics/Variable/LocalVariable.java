22f1d4985ad6188775b078af2914ade57e51cd42
krishna12 <krishyadav7856@gmail.com>
2026-07-10 23:05:01 +0530

    Added Local Variable code

---
+++ Basics/Variable/LocalVariable.java
@@ -0,0 +1,11 @@
+public class LocalVariable {
+
+    public static void display() {
+        int number = 50; // Local variable
+        System.out.println(number);
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
