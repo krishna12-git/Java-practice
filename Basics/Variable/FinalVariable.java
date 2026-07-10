a1b809302547cd96ce863abf4644a7fd7ef7cbee
krishna12 <krishyadav7856@gmail.com>
2026-07-10 23:00:50 +0530

    Added Final Variable Code

---
+++ Basics/Variable/FinalVariable.java
@@ -0,0 +1,10 @@
+public class FinalVariable {
+    public static void main(String[] args) {
+
+        final double PI = 3.14159;
+
+        System.out.println(PI);
+
+        // PI = 5; // Error
+    }
+}
