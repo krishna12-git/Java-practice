394bfb3f7b7070300b0efeb52c47438557d2b378
krishna12 <krishyadav7856@gmail.com>
2026-07-10 22:48:16 +0530

    Added Java Variable files

---
+++ Basics/Variable/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Krishna";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
