28ae9f4be9d333568c08667c75f2a7ff5f7e6a2f
krishna12 <krishyadav7856@gmail.com>
2026-07-11 21:49:47 +0530

    added all example at once

---
+++ Basics/Variable/Data Types/DataTypesdemo.java
@@ -0,0 +1,25 @@
+public class DataTypesdemo {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short year = 2026;
+        int marks = 95;
+        long mobile = 9876543210L;
+
+        float height = 5.9f;
+        double percentage = 89.75;
+
+        char grade = 'A';
+
+        boolean passed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Year: " + year);
+        System.out.println("Marks: " + marks);
+        System.out.println("Mobile: " + mobile);
+        System.out.println("Height: " + height);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + passed);
+    }
+}
