8dd4f2e70a6b015a1aadbd6663d3ddd065a95882
krishna12 <krishyadav7856@gmail.com>
2026-07-11 21:47:28 +0530

    added object example

---
+++ Basics/Variable/Data Types/Student.java
@@ -0,0 +1,13 @@
+// object example
+
+class Student {
+    String name = "Krishna";
+}
+
+public class ObjectExample {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        System.out.println(s1.name);
+    }
+}
