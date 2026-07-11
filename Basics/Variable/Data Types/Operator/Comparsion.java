f093aee3c29a8386c4643603dd6d47f9f1e7e92a
krishna12 <krishyadav7856@gmail.com>
2026-07-12 01:09:06 +0530

    added comparison example

---
+++ Basics/Variable/Data Types/Operator/Comparsion.java
@@ -0,0 +1,13 @@
+public class Comparsion{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a > b);  // false
+        System.out.println(a < b);  // true
+        System.out.println(a >= b); // false
+        System.out.println(a <= b); // true
+    }
+}
