d2dd0c8ce0865c0cf6e718b6f8880b1d005ec2da
krishna12 <krishyadav7856@gmail.com>
2026-07-12 01:06:19 +0530

    added example of assignment operator

---
+++ Basics/Variable/Data Types/Operator/Assignment.java
@@ -0,0 +1,17 @@
+public class Assignment{
+    public static void main(String[] args) {
+        int x = 10;
+
+        x += 5;  // x = x + 5
+        System.out.println(x); // 15
+
+        x -= 3;
+        System.out.println(x); // 12
+
+        x *= 2;
+        System.out.println(x); // 24
+
+        x /= 4;
+        System.out.println(x); // 6
+    }
+}
