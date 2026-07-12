e154c0bf53d25cdb04b8048c4b8200ed24e8ced6
krishna12 <krishyadav7856@gmail.com>
2026-07-12 10:58:16 +0530

    added ternary example

---
+++ Operator/Ternary.java
@@ -0,0 +1,9 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int marks = 75;
+
+        String result = (marks >= 40) ? "Pass" : "Fail";
+
+        System.out.println(result);
+    }
+}
