89cfefe0bb87b9e90a6b49011456626c167b47ac
krishna12 <krishyadav7856@gmail.com>
2026-07-12 10:56:11 +0530

    added inrement example

---
+++ Operator/IncrementExample.java
@@ -0,0 +1,13 @@
+public class IncrementExample {
+    public static void main(String[] args) {
+        int a = 5;
+
+        System.out.println(a++); // 5
+        System.out.println(a);   // 6
+
+        System.out.println(++a); // 7
+
+        System.out.println(a--); // 7
+        System.out.println(a);   // 6
+    }
+}
