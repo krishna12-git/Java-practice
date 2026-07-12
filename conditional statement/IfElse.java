c5e910e9cf7d23316470a424687acddc4969d33b
krishna12 <krishyadav7856@gmail.com>
2026-07-12 11:32:01 +0530

    added if else example

---
+++ conditional statement/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int marks = 35;
+
+        if (marks >= 40) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
