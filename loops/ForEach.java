16b2dcfdbe89ef1a30af69148acdd54f527301fd
krishna12 <krishyadav7856@gmail.com>
2026-07-13 23:26:14 +0530

    example of for each loop

---
+++ loops/ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Mango"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
