506e1d60792f23ddb755ce918f80dbb5d2ebc75c
krishna12 <krishyadav7856@gmail.com>
2026-07-15 22:26:51 +0530

    added Method with no parameters and no return value example

---
+++ method/maini1.java
@@ -0,0 +1,13 @@
+// Method with no parameters and no return value
+
+
+public class maini1 {
+
+    static void greet() {
+        System.out.println("Hello, Welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
