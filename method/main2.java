440803cf6bfa763aabc1bf1a6551fd8a5a276448
krishna12 <krishyadav7856@gmail.com>
2026-07-15 22:32:32 +0530

    added  Method with parameters

---
+++ method/main2.java
@@ -0,0 +1,12 @@
+// Method with parameters
+
+public class main2 {
+
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("Krishna");
+    }
+}
