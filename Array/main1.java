855a4d4f12f56043493561b20f4a6f463617d246
krishna12 <krishyadav7856@gmail.com>
2026-07-20 23:25:10 +0530

    started learing array

---
+++ Array/main1.java
@@ -0,0 +1,10 @@
+public class main1 {
+    public static void main(String[] args) {
+
+        int[] marks = {75, 80, 90, 85, 95};
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
