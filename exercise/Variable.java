fb00f8773413242d3082e7d5984e3837d65d1d80
krishna12 <krishyadav7856@gmail.com>
2026-07-25 23:43:29 +0530

    exercise 2

---
+++ exercise/Variable.java
@@ -0,0 +1,17 @@
+public class Variable{
+    public static void main(String[] args){
+        int length = 2;
+        int width = 4;
+        int area = length  * width;
+
+        int perimeter =  2 * (length * width);
+
+        System.out.println("Length = " + length);
+        System.out.println("Width = " + width);
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+
+
+
+    }
+}
