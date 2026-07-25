29f14aaf3e622a1e5d58aeba2d94bb4369b5e8e0
krishna12 <krishyadav7856@gmail.com>
2026-07-25 23:32:25 +0530

    Fix exercise folder and add Print.java

---
+++ exercise/Print.java
@@ -0,0 +1,11 @@
+public class print{
+    public static void main(String[] args) {
+
+         System.out.println(" name: krishna");
+        System.out.println("Age : 20");
+        System.out.println("College: Kirti M. Doongursee College");
+        System.out.println("Course: B.Sc. Information Technology");
+        
+       
+    }
+}
